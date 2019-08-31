0d88e0762cb367e8a0a1200f38a02c40fca5e440
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 01:14:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.length == 0
+    return nil
+  else
+    array = []
+    name_hash.each do |key, value|
+      array << value
+    end
+    array.sort
+    min_value = array[0]
+  end
+  name_hash.each do |key, value| 
+    if value == min_value 
+      return key
+    end
+  end
 end
