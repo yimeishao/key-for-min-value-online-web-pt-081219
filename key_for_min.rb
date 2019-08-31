bad0f80e5725bd826aeb4b2d2aa5fc63ba2ae5ca
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 01:14:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,8 +9,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       array << value
     end
-    array.sort
-    min_value = value if 
+    min_value = value if value 
   end
   name_hash.each do |key, value| 
     if value == min_value 
